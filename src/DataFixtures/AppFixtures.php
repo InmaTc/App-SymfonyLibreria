9777bceed4e67c78e3264c0feba2a18af16168db
InmaTc <inmatc94@gmail.com>
2022-02-21 22:49:37 +0100

    Creado configuración de fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\AutorFactory;
+use App\Factory\EditorialFactory;
+use App\Factory\LibroFactory;
+use App\Factory\SocioFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +13,16 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        AutorFactory::createMany(200);
+        EditorialFactory::createMany(100);
+        SocioFactory::createMany(20);
+        LibroFactory::createMany(50, function (){
+            return [
+              'autores' => AutorFactory::randomRange(1,3),
+              'editorial' => EditorialFactory::random(),
+              'socio' => LibroFactory::faker()->boolean(25) ? SocioFactory::random() : null
+            ];
+        });
 
         $manager->flush();
     }
