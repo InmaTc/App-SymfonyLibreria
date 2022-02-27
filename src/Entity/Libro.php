f3be12dbb2dc9f8fc8c0aaf41d5a57519cf8a0c6
InmaTc <inmatc94@gmail.com>
2022-02-27 13:17:45 +0100

    Modificada entidad Libro para validar los campos en el formulario

--- src/Entity/Libro.php
+++ src/Entity/Libro.php
@@ -5,9 +5,11 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Repository\LibroRepository;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass=LibroRepository::class)
  * @ORM\Table(name="libro")
  */
 class Libro
@@ -23,48 +25,62 @@ class Libro
     /**
      * @ORM\ManyToOne(targetEntity="Editorial", inversedBy="libros")
      * @ORM\JoinColumn(nullable=false)
+     * @Assert\NotNull()
      * @var Editorial
      */
     private $editorial;
 
     /**
      * @ORM\Column(type="string")
+     * @Assert\NotBlank(message="Debe introducir el título")
+     * @Assert\Length(min=3, minMessage="Mínimo tres letras")
      * @var string
      */
     private $titulo;
 
     /**
      * @ORM\ManyToMany(targetEntity="Autor", inversedBy="libros")
+     * @Assert\NotNull()
      * @var Autor[]|Collection
      */
     private $autores;
 
     /**
      * @ORM\Column(type="integer")
+     * @Assert\NotBlank(message="Debe indicar el año de publicación")
+     * @Assert\Range(min=1500, max=2022, notInRangeMessage="El año debe estar entre 1500 y la actualidad")
      * @var int
      */
     private $anioPublicacion;
 
     /**
      * @ORM\Column(type="string", unique=true)
+     * @Assert\NotBlank(message="Debe introducir el ISBN")
+     * @Assert\Isbn(type="isbn10", message="ISBN no válido")
      * @var string
      */
     private $isbn;
 
     /**
      * @ORM\Column(type="integer", nullable=true)
+     * @Assert\NotBlank(message="Introduzca el número de páginas del libro")
+     * @Assert\Positive(message="Debe ser un número positivo")
      * @var int
      */
     private $paginas;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="Debe rellenar este campo")
+     * @Assert\Length(min="3", max="240", minMessage="Mínimo tres caracteres", maxMessage="Máximo 240 caracteres")
      * @var string
      */
     private $sinopsis;
 
     /**
      * @ORM\Column(type="integer")
+     * @Assert\NotBlank(message="Introduzca el precio sin decimales")
+     * @Assert\PositiveOrZero(message="Debe ser un número mayor o igual a 0")
      * @var int
      */
     private $precioCompra;
