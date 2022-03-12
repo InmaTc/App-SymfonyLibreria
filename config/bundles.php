f15c78df5e2740b2adceb5e05d99c05ce7895982
InmaTc <inmatc94@gmail.com>
2022-03-12 18:29:15 +0100

    Instalado paquete de seguridad

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
