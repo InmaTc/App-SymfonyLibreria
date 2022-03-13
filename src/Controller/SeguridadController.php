1642769260c4524ecc86aed05e9b295d41d7c9bd
InmaTc <inmatc94@gmail.com>
2022-03-13 13:55:57 +0100

    Creado SecurityControlador para gestionar login y salida

---
+++ src/Controller/SeguridadController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Libro;
+use App\Form\LibroType;
+use App\Repository\LibroRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SeguridadController extends AbstractController
+{
+    /**
+     * @Route("/login", name="seguridad_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'error' => $error,
+            'ultimo_usuario' => $lastUsername
+        ]);
+    }
+
+    /**
+     * @Route("/salir", name="seguridad_logout")
+     */
+    public function salir(): Response
+    {
+        throw new AccessDeniedHttpException();
+    }
+}
